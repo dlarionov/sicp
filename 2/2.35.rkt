21755db61a980b67611c2e5bf8070e134d254ca7
Denis <denis.larionov@gmail.com>
2016-06-17 00:15:01 +0300

    count-leaves

---
+++ 2/2.35.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (count-leaves tree)
+  (accumulate + 
+              0
+              (map (lambda(x) 
+                     (if (pair? x)
+                         (count-leaves x)
+                         1))
+                   tree)))
+
+(count-leaves (list 1 2 (list 4 (list) 5) 3))
