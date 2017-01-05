d9f3e9b7c625e79b615ebf92c3c08f56fb0fe783
Denis <denis.larionov@gmail.com>
2017-01-05 21:31:05 +0300

    union-set

---
+++ 2/2.59.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? (car set) x) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+(define (union-set a b)
+  (if (null? a)
+      b
+      (if (element-of-set? (car a) b)
+          (union-set (cdr a) b)
+          (union-set (cdr a) (cons (car a) b)))))
+
+(union-set '(1 2 3 4) '(3 4 5))
+
+
+
+      
+  
+
+  
