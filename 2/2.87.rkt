33a98ff18c038916bc4517d3caa04ef5a8b716d3
Denis Larionov <denis.larionov@gmail.com>
2017-02-08 15:33:14 +0300

    polynomial zero?

--- 2/2.87.rkt
+++ 2/2.87.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+; arithmetical package
+
 (define (square x) (* x x))
 
 (define table (make-hash))
@@ -62,16 +64,15 @@
           (if (> (length types) 1)              
               (let ((result2 (apply-generic-internal (raise-args args))))
                 (if (null? result2)                    
-                    (error "No method for these types")
+                    (error "No method for these types" (cons op args))
                     result2))
-              (error "No method for these types")))
+              (error "No method for these types" (cons op args))))
         result1)))
 
 (define (make-integer x) ((get 'make 'integer) x))
 (define (make-rational n d) ((get 'make 'rational) n d))
 (define (make-real x) ((get 'make 'real) x))
-(define (make-complex-from-real-imag x y) ((get 'make-from-real-imag 'complex) x y))
-(define (make-complex-from-mag-ang r a) ((get 'make-from-mag-ang 'complex) r a))
+(define (make-complex x y) ((get 'make-from-real-imag 'complex) x y))
 
 (define (install-integer-package)
   (define (tag x) (attach-tag 'integer x))
@@ -121,7 +122,7 @@
   (put 'div '(real real) (lambda (x y) (tag (/ x y))))
   (put 'equ? '(real real) (lambda (x y) (= x y)))
   (put 'zero? '(real) (lambda (x) (= x 0)))
-  (put 'raise '(real) (lambda (x) (make-complex-from-real-imag x 0)))
+  (put 'raise '(real) (lambda (x) (make-complex x 0)))
   (put 'project '(real) (lambda(x)
                           (if (= (round x) x)
                               (make-integer x)
@@ -189,6 +190,8 @@
   (put 'real-part '(complex) real-part)
   (put 'imag-part '(complex) imag-part)
   (put 'magnitude '(complex) magnitude)
+  (put 'equ? '(complex complex) (lambda (x y) (and (= (real-part x) (real-part y)) (= (imag-part x) (imag-part y)))))
+  (put 'zero? '(complex) (lambda (x) (= (magnitude x) 0)))
   (put 'angle '(complex) angle)
   (put 'raise '(complex) (lambda (x) (tag x)))
   (put 'project '(complex) (lambda (x)
@@ -201,3 +204,106 @@
 (install-real-package)
 (install-rational-package)
 (install-complex-package)
+
+; 2.87 exercise
+(define (variable? x) (symbol? x))
+(define (same-variable? x y) (and (variable? x) (variable? y) (eq? x y)))
+
+(define (install-polynomial-package)
+  (define (make-poly variable term-list) (cons variable term-list))
+  (define (variable p) (car p))
+  (define (term-list p) (cdr p))
+
+  (define (adjoin-term term term-list)
+    (if (zero? (coeff term))
+        term-list
+        (cons term term-list)))
+  (define (the-empty-termlist) '())
+  (define (first-term term-list) (car term-list))
+  (define (rest-terms term-list) (cdr term-list))
+  (define (empty-termlist? term-list) (null? term-list))
+  (define (make-term order coeff) (list order coeff))
+  (define (order term) (car term))
+  (define (coeff term) (cadr term))
+
+  (define (add-terms L1 L2)
+    (cond ((empty-termlist? L1) L2)
+          ((empty-termlist? L2) L1)
+          (else
+           (let ((t1 (first-term L1)) 
+                 (t2 (first-term L2)))
+             (cond ((> (order t1) (order t2)) (adjoin-term t1 (add-terms (rest-terms L1) L2)))
+                   ((< (order t1) (order t2)) (adjoin-term t2 (add-terms L1 (rest-terms L2))))
+                   (else (adjoin-term
+                          (make-term (order t1) (add (coeff t1) (coeff t2)))
+                          (add-terms (rest-terms L1) (rest-terms L2)))))))))
+  
+  (define (mul-terms L1 L2)
+    (if (empty-termlist? L1)
+        (the-empty-termlist)
+        (add-terms 
+         (mul-term-by-all-terms (first-term L1) L2)
+         (mul-terms (rest-terms L1) L2))))
+
+  (define (mul-term-by-all-terms t1 L)
+    (if (empty-termlist? L)
+        (the-empty-termlist)
+        (let ((t2 (first-term L)))
+          (adjoin-term
+           (make-term (+ (order t1) (order t2)) (mul (coeff t1) (coeff t2)))
+           (mul-term-by-all-terms t1 (rest-terms L))))))
+
+  (define (add-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1) (add-terms (term-list p1) (term-list p2)))
+        (error "variables are not the same")))
+  (define (mul-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1) (mul-terms (term-list p1) (term-list p2)))
+        (error "variables are not the same")))
+  
+  (define (tag x) (attach-tag 'polynomial x))
+  (put 'make 'polynomial (lambda (var terms) (tag (make-poly var terms))))
+  (put 'add '(polynomial polynomial) (lambda (p1 p2) (tag (add-poly p1 p2))))
+  (put 'mul '(polynomial polynomial) (lambda (p1 p2) (tag (mul-poly p1 p2))))
+  (put 'zero? '(polynomial) (lambda (x) (empty-termlist? (term-list x))))
+  )
+
+(install-polynomial-package)
+
+(define (make-polynomial var terms)
+  ((get 'make 'polynomial) var terms))
+
+(define x1 (make-polynomial
+            'x
+            (list
+             (list 2 3)
+             (list 1 (make-complex 2 3))
+             (list 0 7))))
+
+(define x2 (make-polynomial
+            'x
+            (list
+             (list 4 1)
+             (list 2 (make-rational 2 3))
+             (list 0 (make-complex 5 3)))))
+
+(add x1 x2)
+(mul x1 x2)
+
+(define y1 (make-polynomial
+            'x
+            (list
+             (list 2 3)
+             (list 1 (make-complex 2 3))
+             (list 0 7))))
+
+(define y2 (make-polynomial
+            'x
+            (list
+             (list 4 1)
+             (list 2 (make-rational 2 3))
+             (list 0 (make-complex 5 3)))))
+
+(add y1 y2)
+(mul y1 y2)
